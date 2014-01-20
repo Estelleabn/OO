791992cb3d7f01dc783a8d16edd3db54e3de9aa5
Estelleabn <estelleaubouin@gmail.com>
2014-01-20 14:25:04 +0100

    solution OO-exo-06

--- 06-Instance-vs-Class/restaurant.rb
+++ 06-Instance-vs-Class/restaurant.rb
@@ -1,3 +1,58 @@
+
+
 class Restaurant
-  # Your turn !
+  	@total_ratings = 0
+    attr_reader :rating
+	
+	class << self
+		attr_accessor :total_ratings
+	end
+
+	def self.global_ratings
+	  "#{@total_ratings} restaurants were rated by our app. SWEET!"
+    end
+
+    def self.add_global_ratings
+	  @total_ratings += 1
+    end
+  
+  def initialize(name, address, type_of_food)
+  	@name = name
+  	@address = address
+  	@rating = []
+  	@type_of_food = type_of_food
+  	@ambiancy = []
+  end
+
+  def rate(rating)
+  	if rating <= 5
+  	@rating << rating
+  	Restaurant.add_global_ratings
+  else
+  	puts "You can only rate between 0 and 5"
+  end
+  end
+
+  def avg_rating
+  	@avg_rating = ( (@rating.inject(:+).to_f) / @rating.length ).round(1)
+  end
+
+  def add_ambiancy(ambiancy)
+    @ambiancy << ambiancy
+  end
+
+  def to_s
+  	"The Restaurant #{@name}, #{@address}, has an average rate of #{@avg_rating} / 5"
+  end
+
 end
+
+bec_fin = Restaurant.new("Le Bec Fin", "12 rue Saint Georges, 78400 Chatou", :bistrot)
+bec_fin.rate(2)
+bec_fin.rate(3)
+bec_fin.rate(6)
+bec_fin.avg_rating
+puts bec_fin
+bec_fin.add_ambiancy(:sympa)
+puts Restaurant.global_ratings
+puts Restaurant.total_ratings
